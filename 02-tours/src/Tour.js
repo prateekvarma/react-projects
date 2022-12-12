5f294a91be5c2d0dd43245d12263a4d42be906fc
Prateek Varma <prtk.varma@gmail.com>
2022-12-12 13:21:03 -0600

    Toggle 'read more' and 'show less' button

--- 02-tours/src/Tour.js
+++ 02-tours/src/Tour.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false)
+
   return (
     <article className='single-tour'>
       <img src={image} alt={name} />
@@ -9,7 +11,11 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className='tour-price'>${price}</h4>
         </div>
-        <p>{info}</p>
+        <p>{readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'Show Less' : 'Read More'}
+          </button>
+        </p>
         <button className='delete-btn'>Not interested</button>
       </footer>
     </article>
