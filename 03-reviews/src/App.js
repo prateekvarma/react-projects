5ed4ec285dcd2cbd6f384b3e43efae9988d5273b
Prateek Varma <prtk.varma@gmail.com>
2022-12-12 14:54:27 -0600

    Making the accordion work

--- 03-reviews/src/App.js
+++ 03-reviews/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import Review from './Review';
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <section className='container'>
+      <div className='title'>
+        <h2>Our Reviews</h2>
+        <div className='underline'></div>
+      </div>
+      <Review />
+    </section>
+  )
 }
 
 export default App;
--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -3,7 +3,54 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const checkNumber = (n) => {
+    if (n > people.length-1) {
+      return 0
+    } else if (n < 0) {
+      return people.length - 1
+    }
+    return n
+  }
+
+  const nextPerson = () => {
+    setIndex((prevIndex) => {
+      let newIndex = prevIndex + 1 
+      return checkNumber(newIndex)
+    })
+  }
+
+  const prevPerson = () => {
+    setIndex((prevIndex) => {
+      let newIndex = prevIndex - 1 
+      return checkNumber(newIndex)
+    })
+  }
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className="person-img" />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn'>Surprise Me</button>
+    </article>
+  )
 };
 
 export default Review;
