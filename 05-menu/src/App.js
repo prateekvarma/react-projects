b8919959ace76f9651df22ecc7fa4e86e4b59d20
Prateek Varma <prtk.varma@gmail.com>
2022-12-14 21:33:08 -0600

    05 Menu Project initial setup

---
+++ 05-menu/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+
+  return(
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu />
+      </section>
+    </main>
+  );
+}
+
+export default App;
