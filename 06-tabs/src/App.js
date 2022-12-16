d0bbac66d4d857b0bf1a3ca9675940d3c9b23734
Prateek Varma <prtk.varma@gmail.com>
2022-12-16 12:40:28 -0600

    setup to fetch data from API

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -2,8 +2,32 @@ import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
 
 const url = 'https://course-api.com/react-tabs-project'
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const response = await fetch(url)
+    const newJobs = await response.json()
+    setJobs(newJobs)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+
+  if(loading) {
+    return (
+      <section className='section loading'>
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
+
+  return <h2>jobs</h2>
 }
 
 export default App
