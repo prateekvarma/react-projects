834fad6a6a288c329d666369f1d039435d9395d9
Prateek Varma <prtk.varma@gmail.com>
2022-12-19 16:46:22 -0600

    setup component structure and states

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
 import data from './data';
+
 function App() {
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] =  useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDeafult();
+    console.log('Handle Submit');
+  }
   
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className="section-center">
+    <h3>tired of boring lorem ipsum?</h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      <label htmlFor="amount">paragraphs:</label>
+      <input type="number" id="amount" value={count} onChange={(e) => setCount(e.target.value)} />
+      <button type="submit" className='btn'>Generate</button>
+    </form>
+    <article className='lorem-text'>
+      {/* <p>generated text goes here</p> */}
+    </article>
+  </section>
     )
 }
 
