5c06a75da3450723a5e7588389cee2249cb6f61a
Prateek Varma <prtk.varma@gmail.com>
2022-12-20 12:13:57 -0600

    Setup for all states and html elements

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -1,10 +1,32 @@
 import React, { useState } from 'react'
 import SingleColor from './SingleColor'
 
-import Values from 'values.js'
+import Values from 'values.js' // from the library
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('handle submut')
+  }
+
+  return (
+    <>
+      <section className='container'>
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={color} onChange={(e) => setColor(e.event.target.value)} placeholder="#f15025" />
+          <button type="submit" className='btn'>Submit</button>
+        </form>
+      </section>
+      <section className='colors'>
+        <h4>list goes here</h4>
+      </section>
+    </>
+  )
 }
 
 export default App
