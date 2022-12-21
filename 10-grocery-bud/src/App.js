c95db7398c86fff3a20f0803f27cfa5d44e99800
Prateek Varma <prtk.varma@gmail.com>
2022-12-21 12:19:17 -0600

    State setup on App component, and html structures

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -3,7 +3,37 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editId, setEditId] = useState(null)
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("handle submit")
+  }
+
+  return (
+    <section className='section-center'>
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery Bud</h3>
+        <div className='form-control'>
+          <input value={name} onChange={(e) => setName(e.target.value)} type="text" className='grocery' placeholder='eg. eggs' />
+          <button type="submit" className='submit-btn'>
+            {isEditing ? 'Edit' : 'Submit'}
+          </button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn'>
+          clear items
+        </button>
+      </div>
+    </section>
+  )
 }
 
 export default App
