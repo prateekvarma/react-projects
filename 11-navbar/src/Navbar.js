086c9bf0c23bc38c385ad61d583748f9e0d0c352
Prateek Varma <prtk.varma@gmail.com>
2022-12-27 15:20:12 -0600

    Toggle navbar links on clicking hamburger icon

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -4,27 +4,31 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false)
+
   return (
     <nav>
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} alt='logo' />
-          <button className='nav-toggle'>
+          <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
             <FaBars />
           </button>
         </div>
+        { showLinks && (
           <div className='links-container show-container'>
-            <ul className='links'>
-              {links.map((link) => {
-                const { id, url, text } = link;
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+          <ul className='links'>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        ) }
           <ul className='social-icons'>
             {social.map((socialIcon) => {
               const { id, url, icon } = socialIcon
