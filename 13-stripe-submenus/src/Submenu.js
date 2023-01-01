1161e671b94258e935214ec53b4bdb5d2fd3d0c0
Prateek Varma <prtk.varma@gmail.com>
2023-01-01 15:13:01 -0600

    Dynamic changes to the no of columns according to the no of links

--- 13-stripe-submenus/src/Submenu.js
+++ 13-stripe-submenus/src/Submenu.js
@@ -5,18 +5,28 @@ import { useGlobalContext } from './context'
 const Submenu = () => {
   const { isSubmenuOpen, location, page: { page, links } } = useGlobalContext()
   const container = useRef(null)
+  const [columns, setColumns] = useState('col-2')
 
   useEffect(() => {
+    setColumns('col-2') //default as 2 columns
     const submenu = container.current
     const { center, bottom } = location //coming from the context API
     submenu.style.left = `${center}px`
     submenu.style.top = `${bottom}px`
-  }, [location])
+
+    if(links.length === 3) {
+      setColumns('col-3')
+    }
+    if(links.length > 3) { //if there are more than 3 links, keep a max of 4 columns
+      setColumns('col-4')
+    }
+
+  }, [location, links])
 
   return (
     <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu' }`} ref={container}>
       <h4>{page}</h4>
-      <div className={`submenu-center col-2`}>
+      <div className={`submenu-center ${columns}`}>
         {links.map((link, index) => {
           const { label, icon, url } = link
           return <a key={index} href={url}>{icon} {label}</a>
