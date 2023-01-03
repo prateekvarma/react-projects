4a68339c8a553d51460a9eefedfef5d2b2b99731
Prateek Varma <prtk.varma@gmail.com>
2023-01-03 12:57:35 -0600

    Increase item count function

--- 14-cart/src/reducer.js
+++ 14-cart/src/reducer.js
@@ -10,6 +10,16 @@ const reducer = (state, action) => {
         }
     }
 
+    if(action.type === 'INCREASE') {
+        let tempCart = state.cart.map((cartItem) => {
+            if(cartItem.id === action.payload) {
+                return { ...cartItem, amount: cartItem.amount + 1 }
+            }
+            return cartItem
+        })
+        return {...state, cart: tempCart}
+    }
+
     return state
 }
 
