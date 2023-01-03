0410b925ca73ecfe769c9153f1c840b45705db29
Prateek Varma <prtk.varma@gmail.com>
2023-01-03 16:07:19 -0600

    UseEffect and reduce() to calculate cart total & amount

--- 14-cart/src/reducer.js
+++ 14-cart/src/reducer.js
@@ -30,6 +30,20 @@ const reducer = (state, action) => {
         return {...state, cart: tempCart}
     }
 
+    if(action.type === 'GET_TOTAL') {
+        let { total, amount } = state.cart.reduce((cartTotal, cartItem) => {
+            const { price, amount } = cartItem;
+            const itemTotal = price * amount
+            cartTotal.total += itemTotal
+            cartTotal.amount += amount
+            return cartTotal
+        }, {total: 0, amount: 0}) //initial values
+
+        total = parseFloat(total.toFixed(2)) //completely optional line: limits total value to 2 decimal places
+
+        return { ...state, total, amount }
+    }
+
     return state
 }
 
