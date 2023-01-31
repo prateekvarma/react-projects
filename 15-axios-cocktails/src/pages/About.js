e0707e750010a04671fb2d3753a5da77d2c8be62
Prateek Varma <prtk.varma@gmail.com>
2023-01-31 13:10:59 -0600

    Structured the About and Error pages

--- 15-axios-cocktails/src/pages/About.js
+++ 15-axios-cocktails/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Nunc vel risus commodo viverra maecenas. Scelerisque in dictum non consectetur. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Sit amet aliquam id diam maecenas. Maecenas accumsan lacus vel facilisis volutpat est velit egestas. Vitae turpis massa sed elementum tempus. Elementum eu facilisis sed odio. Tincidunt vitae semper quis lectus nulla at volutpat diam. Fringilla est ullamcorper eget nulla facilisi etiam. Sit amet mattis vulputate enim nulla aliquet porttitor lacus.</p>
+    </section>
   )
 }
 
