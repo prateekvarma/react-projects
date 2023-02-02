2a0d28ed46f3d160c2786e6c6a5270cc53c4a5e8
Prateek Varma <prtk.varma@gmail.com>
2023-02-02 16:25:37 -0600

    Search functionality

--- 15-axios-cocktails/src/components/SearchForm.js
+++ 15-axios-cocktails/src/components/SearchForm.js
@@ -3,11 +3,21 @@ import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext() //extracting this function in order to invoke it in parent from child.
+  const searchValue = React.useRef('');
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  }
 
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className='section search'>
+      <form className='search-form'>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input type='text' id='name' ref={searchValue} onChange={searchCocktail} />
+        </div>
+      </form>
+    </section>
   )
 }
 
