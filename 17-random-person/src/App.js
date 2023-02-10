741acda8ebbbeb1b2c072c187f6a58f50a047b7d
prateekvarma <prtk.varma@gmail.com>
2023-02-10 13:53:38 -0600

    Extracting value from data-dash-ID and accessing it using events

--- 17-random-person/src/App.js
+++ 17-random-person/src/App.js
@@ -55,7 +55,10 @@ function App() {
   }, []);
 
   const handleValue = (e) => {
-    console.log(e.target);
+    if (e.target.classList.contains('icon')) {
+      const newValue = e.target.dataset.label;
+      console.log(newValue);
+    }
   };
 
   return (
