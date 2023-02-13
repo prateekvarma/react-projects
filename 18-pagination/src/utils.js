65760bb280c8e57d0bbe742436c14628c70ad4ac
prateekvarma <prtk.varma@gmail.com>
2023-02-13 15:50:12 -0600

    Pagination logic

--- 18-pagination/src/utils.js
+++ 18-pagination/src/utils.js
@@ -1,3 +1,13 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 9;
+  const pages = Math.ceil(followers.length / itemsPerPage);
 
-export default paginate
+  const newFollowers = Array.from({ length: pages }, (_, i) => {
+    const start = i * itemsPerPage;
+    return followers.slice(start, start + itemsPerPage);
+  });
+
+  return newFollowers;
+};
+
+export default paginate;
