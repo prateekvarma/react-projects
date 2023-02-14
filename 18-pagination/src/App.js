fe3e79a780f6aa363ad4b2aefaad4d8efdd24444
prateekvarma <prtk.varma@gmail.com>
2023-02-14 15:59:23 -0600

    Using array of arrays from the paginate function to display data on main component

--- 18-pagination/src/App.js
+++ 18-pagination/src/App.js
@@ -4,6 +4,16 @@ import Follower from './Follower';
 
 function App() {
   const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    if (loading) {
+      return;
+    } 
+
+    setFollowers(data[page]);
+  }, [loading]);
 
   return (
     <main>
@@ -13,7 +23,7 @@ function App() {
       </div>
       <section className='followers'>
         <div className='container'>
-          {data.map((follower) => {
+          {followers.map((follower) => {
             return <Follower key={follower.id} {...follower} />;
           })}
         </div>
