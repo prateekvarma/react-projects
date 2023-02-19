b95ba8f493281b8e44db7e876170d8a8af310854
prateekvarma <prtk.varma@gmail.com>
2023-02-19 17:10:57 -0600

    Displayed images

--- 19-stock-photos/src/App.js
+++ 19-stock-photos/src/App.js
@@ -46,6 +46,14 @@ function App() {
           </button>
         </form>
       </section>
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((image) => {
+            return <Photo key={image.id} {...image} />
+          })}
+        </div>
+        {loading && <h2 className='loading'>Loading...</h2>}
+      </section>
     </main>
   );
 }
