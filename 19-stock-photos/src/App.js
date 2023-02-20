634142182cc55086cb354ac6052adcfbdc97fca3
prateekvarma <prtk.varma@gmail.com>
2023-02-20 15:16:46 -0600

    Fetching and displaying new set of images, when scrolled to the bottom of document

--- 19-stock-photos/src/App.js
+++ 19-stock-photos/src/App.js
@@ -10,16 +10,18 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
 
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}${clientID}`;
+    const urlPage = `&page=${page}`;
+    url = `${mainUrl}${clientID}${urlPage}`;
 
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setPhotos(data);
+      setPhotos((oldPhotos) => [...oldPhotos, ...data]);
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -29,7 +31,7 @@ function App() {
 
   useEffect(() => {
     fetchImages();
-  }, []);
+  }, [page]);
 
   useEffect(() => {
     const event = window.addEventListener('scroll', () => {
@@ -43,6 +45,7 @@ function App() {
       ) {
         //trigger 2 pixels before reaching the end of document
         console.log('Scroll event triggered!');
+        setPage((prevState) => prevState + 1);
       }
     });
 
