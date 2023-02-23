0bdef6bff76676752c94eadae4c12cc49f4715c2
prateekvarma <prtk.varma@gmail.com>
2023-02-23 17:18:27 -0600

    Refactor part 2 : created another useEffect

--- 19-stock-photos/src/App.js
+++ 19-stock-photos/src/App.js
@@ -13,6 +13,7 @@ function App() {
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState('');
   const mounted = useRef(false);
+  const [newImages, setNewImages] = useState(false);
 
   const fetchImages = async () => {
     setLoading(true);
@@ -39,8 +40,10 @@ function App() {
           return [...oldPhotos, ...data]; //standard data
         }
       });
+      setNewImages(false);
       setLoading(false);
     } catch (error) {
+      setNewImages(false);
       setLoading(false);
       console.log(error);
     }
@@ -57,6 +60,21 @@ function App() {
       return;
     }
     //below code never runs on 1st render, but every other re-render
+    if (!newImages) return;
+    if (loading) return;
+    setPage((oldPage) => oldPage + 1);
+  }, [newImages]);
+
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', event);
+
+    return () => window.removeEventListener('scroll', event);
   }, []);
 
   const handleSubmit = (e) => {
