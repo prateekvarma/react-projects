b4bc562abca455dfbdbe818f82cde00129dc87bb
prateekvarma <prtk.varma@gmail.com>
2023-02-28 14:17:04 -0600

    Basic React Router 5 setup, and test console logged value from the context API

--- 21-movie-db/src/App.js
+++ 21-movie-db/src/App.js
@@ -5,7 +5,14 @@ import Home from './Home'
 import Movie from './SingleMovie'
 
 function App() {
-  return <h2>movie DB starter</h2>
+  return (
+    <Switch>
+      <Route path='/' exact>
+        <Home />
+        <Route path='/movies/:id' children={<Movie />}></Route>
+      </Route>
+    </Switch>
+  )
 }
 
 export default App
