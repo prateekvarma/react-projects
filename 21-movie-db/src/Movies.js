18db42ab86f4894587a281dc287cbcf3fd523a95
prateekvarma <prtk.varma@gmail.com>
2023-03-01 15:09:48 -0600

    Displaying data from the Context API as movies

--- 21-movie-db/src/Movies.js
+++ 21-movie-db/src/Movies.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import { useGlobalContext } from './context'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { useGlobalContext } from './context';
+import { Link } from 'react-router-dom';
 
-const url = 'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+const url =
+  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png';
 
 const Movies = () => {
-  const data = useGlobalContext()
-  console.log(data);
+  const { movies, isLoading } = useGlobalContext();
 
-  return <h2>movies component</h2>
-}
+  if (isLoading) {
+    return <div className='loading'></div>;
+  }
 
-export default Movies
+  return (
+    <section className='movies'>
+      {movies.map((movie) => {
+        const { imdbID: id, Poster: poster, Title: title, Year: year } = movie;
+        return (
+          <Link to={`/movies/${id}`} key={id} className='movie'>
+            <article>
+              <img src={poster} alt={title} />
+              <div className='movie-info'>
+                <h4 className='title'>{title}</h4>
+                <p>{year}</p>
+              </div>
+            </article>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Movies;
