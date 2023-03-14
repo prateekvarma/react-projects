c98122ec9a6b779a47fe65977242e239d8aa175e
prateekvarma <prtk.varma@gmail.com>
2023-03-14 14:18:56 -0600

    Displaying questions

--- 23-quiz/src/App.js
+++ 23-quiz/src/App.js
@@ -8,15 +8,40 @@ import Modal from './Modal';
 function App() {
   const { waiting, loading, questions, index, correct } = useGlobalContext();
 
-  if(waiting) {
-    return <SetupForm />
+  if (waiting) {
+    return <SetupForm />;
   }
-  if(loading) {
-    return <Loading />
+  if (loading) {
+    return <Loading />;
   }
+  const { question, incorrect_answers, correct_answer } = questions[0];
+  const answers = [...incorrect_answers, correct_answer];
+  console.log('correct: ', correct);
   return (
-    <main>quiz app</main>
-  )
+    <main>
+      {/* <Modal /> */}
+      <section className='quiz'>
+        <p className='correct-answers'>
+          correct answers: {correct}/{index}
+        </p>
+        <article className='container'>
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className='btn-container'>
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className='answer-btn'
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                />
+              );
+            })}
+          </div>
+        </article>
+        <button className='next-question'>next question</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 23-quiz/src/context.js
+++ 23-quiz/src/context.js
@@ -20,7 +20,7 @@ const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
   const [index, setIndex] = useState(0);
-  const [correct, setCorrect] = useState();
+  const [correct, setCorrect] = useState(0);
   const [error, setError] = useState(false);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
