df4136b5b3679a79a2e10ac5811b898e1e2968c8
prateekvarma <prtk.varma@gmail.com>
2023-03-17 11:44:11 -0600

    Setup form to take inputs, and related context API states

--- 23-quiz/src/SetupForm.js
+++ 23-quiz/src/SetupForm.js
@@ -1,8 +1,32 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from 'react';
+import { useGlobalContext } from './context';
 
 const SetupForm = () => {
-  return <h2>setup form</h2>
-}
+  const { quiz, handleChange, handleSubmit, error } = useGlobalContext();
 
-export default SetupForm
+  return (
+    <section className='quiz quiz-ball'>
+      <form className='setup-form'>
+        <h2>setup quiz</h2>
+        {/* amount */}
+        <div className='form-control'>
+          <label htmlFor='amount'>number of questions</label>
+          <input
+            type='number'
+            name='amount'
+            id='amount'
+            value={quiz.amount}
+            onChange={handleChange}
+            className='form-input'
+            min={1}
+            max={50}
+          />
+        </div>
+        {error && <p className='error'>can't generate questions, please try different options</p>}
+        <button type='submit' onClick={handleSubmit} className='submit-btn'>start</button>
+      </form>
+    </section>
+  );
+};
+
+export default SetupForm;
--- 23-quiz/src/context.js
+++ 23-quiz/src/context.js
@@ -22,6 +22,11 @@ const AppProvider = ({ children }) => {
   const [index, setIndex] = useState(0);
   const [correct, setCorrect] = useState(0);
   const [error, setError] = useState(false);
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    category: 'sports',
+    difficulty: 'easy',
+  });
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -74,9 +79,13 @@ const AppProvider = ({ children }) => {
     setIsModalOpen(false);
   };
 
-  useEffect(() => {
-    fetchQuestions(tempUrl);
-  }, []);
+  const handleChange = (e) => {
+    console.log(e);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
 
   return (
     <AppContext.Provider
@@ -90,7 +99,10 @@ const AppProvider = ({ children }) => {
         isModalOpen,
         nextQuestion,
         checkAnswer,
-        closeModal
+        closeModal,
+        quiz,
+        handleChange,
+        handleSubmit
       }}
     >
       {children}
